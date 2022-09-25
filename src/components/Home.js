ffc67d23b1b9f58f9b2f18e17ca07b444cbd2f14
Brendan Leal <leal.brendan@gmail.com>
2022-09-25 16:23:51 -0700

    Extract Home component from App.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import SpotifyWebPlayer from "./SpotifyWebPlayer";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <SpotifyWebPlayer />
+    </div>
+  );
+}
+
+export default Home;
